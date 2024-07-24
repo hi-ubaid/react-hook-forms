ad9e10744b1692b09f8e0a174b3e7b3eca55b284
Muhammad Ubaid <79584441+hi-ubaid@users.noreply.github.com>
2024-07-24 13:27:51 +0500

    Final Commit

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -6,8 +6,47 @@ import { yupResolver } from '@hookform/resolvers/yup';
 
 export const Form = () => {
  
+    // yup.object().shape({
+    //     first_name: yup
+    //       .string()
+    //       .required('Required'),
+    //     last_name: yup
+    //       .string()
+    //       .required('Required'),
+    //     address_1: yup
+    //       .string()
+    //       .required('Required'),
+    //     address_2: yup
+    //       .string(),
+    //     city: yup
+    //       .string()
+    //       .required('Required'),
+    //     state: yup
+    //       .string()
+    //       .required('Required'),
+    //     postcode: yup
+    //       .number()
+    //       .typeError('Invalid Input: numbers please')
+    //       .positive('Must be greater than zero')
+    //       .integer()
+    //       .required('Required'),
+    //     country: yup
+    //       .string()
+    //       .required('Required'),
+    //     // email: yup
+    //     //   .string()
+    //     //   .email()
+    //     //   .required('Required'),
+    //     phone: yup
+    //       .number()
+    //       .typeError('Invalid Input: numbers please')
+    //       .positive('Must be greater than zero')
+    //       .integer()
+    //       .required('Required'),
+    //   });
+
     const schema =      yup.object().shape({
-        fullName:       yup.string().required("Your Full Name is required"),
+        fullName:       yup.string().matches(/^[a-zA-Z ]*$/, "MUST_BE_CHARACTER"),
         email:          yup.string().email("The email is invalid").required("Your Email is required"),
         age:            yup.number().positive().integer("Do not put points, just years").min(12, "You don't pass the age criteria for our website").required("Age is required"),
         password:       yup.string().min(5, "Minimum length should be five").max(16, "You cannot exceed password more than 16 charachters").required("Password is reqired"),
